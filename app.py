d9a4bef3b296019fd53cb0dcc5934d155b5a6639
halima570 <84968500+halima570@users.noreply.github.com>
2023-01-01 23:09:20 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+def Download(link):
+    youtubeObject=YouTube(link)
+    youtubeObject.streams.filter(file_extension='mp3')
+    youtubeObject =youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download("C:/Users/LENOVO/Desktop/python")
+    except:
+        print("error")
+        print("download not complited")
+
+link=input("entrez URL")
+Download(link)
